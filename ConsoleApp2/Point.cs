d14df9ce99f907ec3a666e8607d8197e1854d3ed
James Montemagno <james.montemagno@gmail.com>
2020-10-30 10:38:12 -0700

    Add project files.

---
+++ ConsoleApp2/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Point
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        public Point()
+        {
+
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        void M()
+        {
+            var point = new Point
+            {
+                X = 42,
+                Y = 21
+            };
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person("James", "Montemagno");
+            var p2 = new Person("Maddy", "Leger");
+
+
+            var json = JsonSerializer.Serialize(p1);
+            Console.WriteLine(json);
+            var p3 = JsonSerializer.Deserialize<Person>(json);
+            var isEqual = p1 == p3;
+            Console.WriteLine($"p1 == p3: {isEqual}");
+
+            var (firstName, lastName) = p1;
+            Console.WriteLine($"{lastName}, {firstName}");
+
+            Console.WriteLine($"Is Maddy in chat {IsInChat(p2)}");
+        }
+
+        private static bool IsInChat(Person p) => p switch
+        {
+            ("James", "Montemagno") => true,
+            ("Maddy", "Leger") => true,
+            _ => false
+        };
+    }
+
+    record Person(string FirstName, string LastName);
+
+    //public class Person : IEquatable<Person>
+    //{
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //    public Person (string firstName, string lastName)
+    //    {
+    //        FirstName = firstName;
+    //        LastName = lastName;
+    //    }
+
+    //    public void Deconstruct(out string firstName, out string lastName)
+    //    {
+    //        firstName = FirstName;
+    //        lastName = LastName;
+    //    }
+
+    //    public bool Equals(Person obj)
+    //    {
+    //        return obj is Person person &&
+    //               FirstName == person.FirstName &&
+    //               LastName == person.LastName;
+    //    }
+
+    //    public override int GetHashCode()
+    //    {
+    //        return HashCode.Combine(FirstName, LastName);
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return base.ToString();
+    //    }
+
+    //    public static bool operator ==(Person left, Person right)
+    //    {
+    //        return left.Equals(right);
+    //    }
+
+    //    public static bool operator !=(Person left, Person right)
+    //    {
+    //        return !(left == right);
+    //    }
+
+    //    public override bool Equals(object obj)
+    //    {
+    //        return obj is Person && Equals((Person)obj);
+    //    }
+    //}
+}
